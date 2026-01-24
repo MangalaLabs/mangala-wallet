104265332debdd58ea5cee6b1d1c414198460b74
Leonard Trinh <trinhvandat90399@gmail.com>
2026-01-24 17:45:42 +0700

    update create wallet navigator switch by network type instead

--- features/onboarding/src/commonMain/kotlin/com/mangala/wallet/features/onboarding/domain/navigator/CreateWalletNavigator.kt
+++ features/onboarding/src/commonMain/kotlin/com/mangala/wallet/features/onboarding/domain/navigator/CreateWalletNavigator.kt
@@ -1,6 +1,7 @@
 package com.mangala.wallet.features.onboarding.domain.navigator
 
 import com.mangala.wallet.model.blockchain.BlockchainType
+import com.mangala.wallet.model.blockchain.NetworkType
 import com.mangala.wallet.model.blockchain.PrimaryNetworkConfig
 import com.mangala.wallet.ui.SharedScreen
 
@@ -8,24 +9,31 @@ interface CreateWalletNavigator {
     fun getCreateWalletScreen(): SharedScreen
 }
 
-class AntelopeCreateWalletNavigator: CreateWalletNavigator {
+class AntelopeCreateWalletNavigator : CreateWalletNavigator {
     override fun getCreateWalletScreen(): SharedScreen {
         return SharedScreen.AntelopeCreateAccountV2Screen
     }
 }
 
-class EvmCreateWalletNavigator: CreateWalletNavigator {
+class EvmCreateWalletNavigator : CreateWalletNavigator {
     override fun getCreateWalletScreen(): SharedScreen {
         return SharedScreen.CreateWalletGuideScreen
     }
 }
 
+class BitcoinCreateWalletNavigator : CreateWalletNavigator {
+    override fun getCreateWalletScreen(): SharedScreen {
+        return SharedScreen.BitcoinCreateAccountScreen()
+    }
+}
+
 object CreateWalletNavigatorFactory {
     fun create(blockchainType: BlockchainType = PrimaryNetworkConfig.primaryBlockchain): CreateWalletNavigator {
-        return when (blockchainType) {
-            BlockchainType.Ethereum -> EvmCreateWalletNavigator()
-            BlockchainType.Eos -> AntelopeCreateWalletNavigator()
-            else -> EvmCreateWalletNavigator() // Default fallback
+        return when (blockchainType.networkType) {
+            NetworkType.EVM -> EvmCreateWalletNavigator()
+            NetworkType.ANTELOPE -> AntelopeCreateWalletNavigator()
+            NetworkType.BITCOIN -> BitcoinCreateWalletNavigator()
+            NetworkType.OTHER, NetworkType.UNSUPPORTED -> EvmCreateWalletNavigator()
         }
     }
 }
