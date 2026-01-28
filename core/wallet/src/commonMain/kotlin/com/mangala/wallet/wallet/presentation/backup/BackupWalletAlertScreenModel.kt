0d881c927bb034d9740183bf36f83d711082e9fe
Leonard Trinh <trinhvandat90399@gmail.com>
2026-01-28 13:34:09 +0700

    Update backend options screen

--- core/wallet/src/commonMain/kotlin/com/mangala/wallet/wallet/presentation/backup/BackupWalletAlertScreenModel.kt
+++ core/wallet/src/commonMain/kotlin/com/mangala/wallet/wallet/presentation/backup/BackupWalletAlertScreenModel.kt
@@ -1,12 +1,23 @@
 package com.mangala.wallet.wallet.presentation.backup
 
 import com.mangala.wallet.domain.wallet.usecases.CreateWalletUseCase
-import com.mangala.wallet.model.blockchain.BlockchainType
-import com.mangala.wallet.model.blockchain.NetworkType
 import com.mangala.wallet.ui.utils.screenmodel.BaseScreenModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 
 class BackupWalletAlertScreenModel(
     private val createWalletUseCase: CreateWalletUseCase,
-): BaseScreenModel() {
+) : BaseScreenModel() {
 
+    private val _showRiskDialog = MutableStateFlow(false)
+    val showRiskDialog: StateFlow<Boolean> = _showRiskDialog.asStateFlow()
+
+    fun onRiskButtonClicked() {
+        _showRiskDialog.value = true
+    }
+
+    fun dismissRiskDialog() {
+        _showRiskDialog.value = false
+    }
 }
